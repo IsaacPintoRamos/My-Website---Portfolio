2b6aa43f3af83658d252b26f9f657245b25b933d
isaac <isaacpintoramos5@gmail.com>
2023-02-02 21:31:27 -0300

    add section projects

--- private/assets/js/script-animation.js
+++ private/assets/js/script-animation.js
@@ -1,3 +1,18 @@
 AOS.init({
     duration: 1000
 });
+
+let count = 1
+document.getElementById("card-project-btn-radio-01").checked = true;
+    
+setInterval(function() {
+    nextImage();
+}, 70000000)
+
+function nextImage() {
+    count++
+        if(count > 4) {
+            count = 1
+        }
+        document.getElementById("card-project-btn-radio-0" + count).checked = true;
+};
